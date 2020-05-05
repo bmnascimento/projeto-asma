f710689c410989e487e631f1b4e7766d3404cc2c
Bernardo <b@bmnascimento.com>
2020-05-04 23:31:54 -0300

    made express serve static content

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('Hello World')
-})
+app.use(express.static('build'))
 
 PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
 
