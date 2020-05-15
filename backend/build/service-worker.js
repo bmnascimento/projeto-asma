897d5b20d0a06d53f2a07e697da202188248fdf1
Bernardo <b@bmnascimento.com>
2020-05-15 20:41:00 -0300

    configuraçoes para deploy

--- backend/build/service-worker.js
+++ backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3af5dd814423767987e04fffe6f82953.js"
+  "/precache-manifest.e9f1f5f9f35b1f2c5005e941c0222396.js"
 );
 
 self.addEventListener('message', (event) => {
